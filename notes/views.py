07b00a0f0ba2263d03a8ae308c8898edd8b96d6e
claransalomao <clarans@al.insper.edu.br>
2026-09-10 09:27:47 -0300

    Handout Django

---
+++ notes/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from .models import Note
+
+
+def index(request):
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        Note.objects.create(title=title, content=content)
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
