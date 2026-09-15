5df08703f1eef20f38fbf4bd6c692109a2b39686
claransalomao <clarans@al.insper.edu.br>
2026-09-15 08:35:57 -0300

    Tarefa1

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:note_id>/delete/', views.delete_note, name='delete_note'),
+    path('<int:note_id>/edit/', views.edit_note, name='edit_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note
 
 
@@ -12,3 +12,20 @@ def index(request):
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
+
+def delete_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    note.delete()
+    return redirect('index')
+
+
+def edit_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    if request.method == 'POST':
+        note.title = request.POST.get('titulo')
+        note.content = request.POST.get('detalhes')
+        note.save()
+        return redirect('index')
+    else:
+        return render(request, 'notes/edit.html', {'note': note})
