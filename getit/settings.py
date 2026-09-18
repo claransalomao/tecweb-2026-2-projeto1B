6b06befd5ccefe7b1db138a6532c4325635af18c
claransalomao <clarans@al.insper.edu.br>
2026-09-18 17:42:54 -0300

    Tarefa3

--- getit/settings.py
+++ getit/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'getit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'getit',
+        'USER': 'getituser',
+        'PASSWORD': 'getitsenha',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
